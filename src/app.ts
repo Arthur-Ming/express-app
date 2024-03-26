b3339ccf04e95fa831abedd5ac92102bd8ece917
Arthur Ming <arthurming7@gmail.com>
2024-03-26 06:28:16 +0500

    start app

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+export const app = express()
+
+const bodyParser = express.json();
+
+app.use(bodyParser);
+
+app.get('/', (req, res) => {
+    res.status(200).json({version: '1.0'})
+})
