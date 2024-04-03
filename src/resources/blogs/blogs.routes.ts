9574dfd6805c75d0c9a07e8c55d8a2ad041d2845
Arthur Ming <arthurming7@gmail.com>
2024-04-03 20:59:52 +0500

    add auth

--- src/resources/blogs/blogs.routes.ts
+++ src/resources/blogs/blogs.routes.ts
@@ -3,13 +3,14 @@ import routes from '../../common/routes';
 import { addBlog, deleteBlog, getBlogById, getBlogs, updateBlog } from './blogs.controller';
 
 import { blogsInputValidation } from './blogsInputValidation';
+import { checkAuthorization } from '../../utils/authorization';
 
 const blogsRouter = Router();
 
 blogsRouter.get(routes.blogs, getBlogs);
 blogsRouter.get(routes.blogById, getBlogById);
-blogsRouter.post(routes.blogs, ...blogsInputValidation, addBlog);
-blogsRouter.put(routes.blogById, updateBlog);
-blogsRouter.delete(routes.blogById, deleteBlog);
+blogsRouter.post(routes.blogs, checkAuthorization, ...blogsInputValidation, addBlog);
+blogsRouter.put(routes.blogById, checkAuthorization, ...blogsInputValidation, updateBlog);
+blogsRouter.delete(routes.blogById, checkAuthorization, deleteBlog);
 
 export default blogsRouter;
