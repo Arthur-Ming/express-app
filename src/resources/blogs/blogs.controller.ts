9ff44e41b805624f8517e96164e8baa234e0a5fd
Arthur Ming <arthurming7@gmail.com>
2024-04-06 21:36:06 +0500

    add blogs typing

--- src/resources/blogs/blogs.controller.ts
+++ src/resources/blogs/blogs.controller.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express';
-import { blogsRepository } from './blogs.repository';
+import { BlogsRepository } from './blogs.repository';
+import { BlogInputData, BlogOutputData } from './interfaces';
 
-export const getBlogs = (req: Request, res: Response) => {
+type RequestWithBody<T> = Request<{}, {}, T>;
+type RequestWithParams<T> = Request<T>;
+type RequestWithParamsAndBody<T, Y> = Request<T, {}, Y>;
+type ParamsId = { id: string };
+
+const blogsRepository = new BlogsRepository();
+export const getBlogs = (req: Request, res: Response<BlogOutputData[]>) => {
   res.status(200).json(blogsRepository.find());
 };
 
-export const getBlogById = (req: Request, res: Response) => {
+export const getBlogById = (req: RequestWithParams<ParamsId>, res: Response<BlogOutputData>) => {
   const foundBlog = blogsRepository.findById(req.params.id);
   if (!foundBlog) {
     res.sendStatus(404);
@@ -14,10 +21,10 @@ export const getBlogById = (req: Request, res: Response) => {
   res.status(200).json(foundBlog);
 };
 
-export const addBlog = (req: Request, res: Response) => {
-  const newBlog = blogsRepository.create(req.body);
+export const addBlog = (req: RequestWithBody<BlogInputData>, res: Response<BlogOutputData>) => {
+  const { id: newBlogId } = blogsRepository.create(req.body);
 
-  const foundBlog = blogsRepository.findById(newBlog.id);
+  const foundBlog = blogsRepository.findById(newBlogId);
   if (!foundBlog) {
     res.sendStatus(404);
     return;
@@ -25,7 +32,10 @@ export const addBlog = (req: Request, res: Response) => {
   res.status(201).json(foundBlog);
 };
 
-export const updateBlog = (req: Request, res: Response) => {
+export const updateBlog = (
+  req: RequestWithParamsAndBody<ParamsId, BlogInputData>,
+  res: Response<void>
+) => {
   if (!blogsRepository.update(req.params.id, req.body)) {
     res.sendStatus(404);
     return;
@@ -33,7 +43,7 @@ export const updateBlog = (req: Request, res: Response) => {
   res.sendStatus(204);
 };
 
-export const deleteBlog = (req: Request, res: Response) => {
+export const deleteBlog = (req: RequestWithParams<ParamsId>, res: Response<void>) => {
   if (!blogsRepository.remove(req.params.id)) {
     res.sendStatus(404);
     return;
