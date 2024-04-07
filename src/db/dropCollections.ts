632153b5a485e2a74ab5ace8505c900e41f6da32
Arthur Ming <arthurming7@gmail.com>
2024-04-07 14:12:04 +0500

    fix drop posts

--- src/db/dropCollections.ts
+++ src/db/dropCollections.ts
@@ -1,5 +1,7 @@
 import { blogCollection } from './blog.collection';
+import { postCollection } from './post.collection';
 
 export const dropCollections = async () => {
   await blogCollection.drop();
+  await postCollection.drop();
 };
