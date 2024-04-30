fea4c05cd95f66ff8db38792be19b8e427944107
Arthur Ming <arthurming7@gmail.com>
2024-04-30 10:24:25 +0500

    fix auth

--- src/resources/auth/auth.routes.ts
+++ src/resources/auth/auth.routes.ts
@@ -2,11 +2,11 @@ import { Router } from 'express';
 import routes from '../../common/routes';
 import { loginBodyValidation } from '../users/usersValidation/loginBodyValidation';
 import { authLogin, authMe } from './auth.controller';
-import { checkAuthorization } from '../../utils/authorization';
+import { checkByJWTAuthorization } from '../../utils/authorization';
 
 const authRouter = Router();
 
-authRouter.get(routes.authMe, checkAuthorization, authMe);
+authRouter.get(routes.authMe, checkByJWTAuthorization, authMe);
 authRouter.post(routes.authLogin, loginBodyValidation, authLogin);
 
 export default authRouter;
