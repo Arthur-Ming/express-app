9ae47296d83e50c53bbcabc16f75ef65232db2da
Arthur Ming <arthurming7@gmail.com>
2024-05-12 16:53:55 +0500

    fix drop emailConfirmationCollection

--- src/db/dropCollections.ts
+++ src/db/dropCollections.ts
@@ -2,10 +2,12 @@ import { blogCollection } from './blog.collection';
 import { postCollection } from './post.collection';
 import { userCollection } from './user.collection';
 import { commentsCollection } from './comments.collection';
+import { emailConfirmationCollection } from './emailConformition.collection';
 
 export const dropCollections = async () => {
   await blogCollection.drop();
   await postCollection.drop();
   await userCollection.drop();
   await commentsCollection.drop();
+  await emailConfirmationCollection.drop();
 };
