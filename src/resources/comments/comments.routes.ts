aa08517001f77ed4a034e67f9e65181d4440466e
Arthur Ming <arthurming7@gmail.com>
2024-06-12 18:18:34 +0500

    fix

--- src/resources/comments/comments.routes.ts
+++ src/resources/comments/comments.routes.ts
@@ -22,13 +22,17 @@ const commentsRouter = Router();
 
 commentsRouter.get(
   routes.commentBySpecifiedPostId,
-
+  /*extractPayloadFromToken,*/
   paramsPostIdValidation,
   commentsQueryParamsValidation,
-
+  // @ts-ignore
   getCommentForSpecifiedPostId
 );
-commentsRouter.get(routes.commentById, extractPayloadFromToken, paramsIdValidation, getCommentById);
+commentsRouter.get(
+  routes.commentById,
+  /*extractPayloadFromToken,*/ paramsIdValidation,
+  getCommentById
+);
 commentsRouter.post(
   routes.commentBySpecifiedPostId,
   checkByJWTAuthorization,
