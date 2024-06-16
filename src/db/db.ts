c630ab01def9e16102b072434e498f607ddce2db
Arthur Ming <arthurming7@gmail.com>
2024-06-16 18:16:18 +0500

    some refactors

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,4 +1,3 @@
-import { Db, MongoClient } from 'mongodb';
 import config from '../common/config';
 import mongoose from 'mongoose';
 
@@ -9,19 +8,14 @@ if (!dbUrl) {
   throw new Error('dbUrl not found!!!!');
 }
 
-//const client: MongoClient = new MongoClient(dbUrl);
-//export const db: Db = client.db(dbName);
-
 export const connectToDB = async () => {
   try {
-    //  await client.connect();
     await mongoose.connect(dbUrl + '/' + dbName);
     console.log('connected to db');
     return true;
   } catch (e) {
-    console.log('db error');
     console.log(e);
-    //await client.close();
+
     await mongoose.disconnect();
     return false;
   }
